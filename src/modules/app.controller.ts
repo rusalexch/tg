73cbaade43823d6d56871a33efc6af651c078710
Alexander Churkin <alex.asutp@gmail.com>
2021-07-28 21:41:59 +0300

    ref: moved common folder into module

--- src/modules/app.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller('app')
-export class AppController {
-  @Get('test')
-  async test() {
-    return { data: 'TEST' };
-  }
-}
--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppLoggerModule } from './app-logger/app-logger.module';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
 
 @Module({
   imports: [AppLoggerModule, ConfigModule.forRoot()],
-  controllers: [AppController],
 })
 export class AppModule {}
