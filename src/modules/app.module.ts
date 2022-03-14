cbe574364e3347db9f678cbf79eb297cc32e8e70
Alexander Churkin <alex.asutp@gmail.com>
2022-03-14 17:39:31 +0300

    feat: init telegram module

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppLoggerModule } from './app-logger/app-logger.module';
 import { ConfigModule } from '@nestjs/config';
-import { config } from '../common/config';
+import {
+  config,
+  TELEGRAM_CONFIG_KEY,
+  TelegramConfigType,
+} from '../common/config';
+import { TelegramModule } from 'nestjs-telegram';
 
 @Module({
-  imports: [AppLoggerModule, ConfigModule.forRoot(config)],
+  imports: [
+    AppLoggerModule,
+    ConfigModule.forRoot(config),
+    TelegramModule.forRootAsync({
+      inject: [TELEGRAM_CONFIG_KEY],
+      useFactory: ({ apiKey }: TelegramConfigType) => ({
+        botKey: apiKey,
+      }),
+    }),
+  ],
 })
 export class AppModule {}
